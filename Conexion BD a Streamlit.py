4dc47676b3eb9b9161df60847bc13038289def9f
Laghi05 <letiziarosalaghi@gmail.com>
2024-07-07 17:16:24 -0400

    Update Conexion BD a Streamlit.py
    En proceso de filtros

--- Conexion BD a Streamlit.py
+++ Conexion BD a Streamlit.py
@@ -76,3 +76,84 @@ if st.button('Actualizar datos'):
     df = cargar_datos()
     st.write(df.head())
 #######################################################################################
+
+import streamlit as st
+import plotly.express as px
+import pandas as pd
+import os
+import warnings
+
+# Ignorar advertencias
+warnings.filterwarnings('ignore')
+
+# Definir titulo de la pagina en navegador, icono y expansion del contenido en la ventana
+
+st.set_page_config(page_title="VIH Satistics", page_icon=":female-doctor: ",layout="wide")
+
+# Titulo de la pagina en la interfaz con icono
+
+st.title(" :female-doctor: VIH en la República Dominicana")
+
+# Definicion de 2.5rem (40 pixeles) de espacio entre el borde superior de cada elemento y su contenido
+
+st.markdown('<style>div.block-container{padding-top:2.5rem;}</style>',unsafe_allow_html=True)
+
+# Crear seccion de filtros en un lado desplegable de la interfaz
+
+st.sidebar.header("Filtros")
+
+# Filtro de region
+
+region = st.sidebar.multiselect(
+    "Seleccionar región",
+    df["Region"].unique()
+)
+
+if not region:
+    df2 = df.copy() # Si no se selecciona region, usar todos los datos del df
+else:
+    df2 = df[df["Region"].isin(region)] # Si hay seleccion, usar los datos donde esta esa region
+
+# Filtro de provincia
+
+provincia = st.sidebar.multiselect("Seleccionar provincia", df2["Provincia"].unique())
+if not provincia:
+    df3 = df2.copy()
+else:
+    df3 = df2[df2["Provincia"].isin(provincia)]
+
+# Filtro de año
+
+anio = st.sidebar.multiselect("Seleccionar año", df3["Año"].unique())
+
+# Coordinar filtros de region, provincia y año
+
+if not region and not provincia and not anio:
+    filtered_df = df
+elif not provincia and not anio:
+    filtered_df = df[df["Region"].isin(region)]
+elif not region and not anio:
+    filtered_df = df[df["Provincia"].isin(provincia)]
+elif provincia and anio:
+    filtered_df = df3[df["Provincia"].isin(provincia) & df3["Año"].isin(anio)]
+elif region and anio:
+    filtered_df = df3[df["Region"].isin(region) & df3["Año"].isin(anio)]
+elif region and provincia:
+    filtered_df = df3[df["Region"].isin(region) & df3["Provincia"].isin(provincia)]
+elif anio:
+    filtered_df = df3[df3["Año"].isin(anio)]
+else:
+    filtered_df = df3[df3["Region"].isin(region) & df3["Provincia"].isin(provincia) & df3["Año"].isin(anio)]
+
+# Los casos se suman y se agrupan por provincia
+category_df = df3.groupby(by = ["Provincia"], as_index = False)["Cant_Casos"].sum()
+
+# Definicion de dos columnas en la interfaz
+col1, col2 = st.columns((2))
+
+# Grafica de barras
+
+st.subheader("Casos por provincia")
+fig = px.bar(category_df, x = "Provincia", y = "Cant_Casos", text = ['{:,.2f}'.format(int(x.replace(",", ""))) for x in category_df["Cant_Casos"]],
+            template = "seaborn")
+st.plotly_chart(fig,use_container_width=True, height = 200)
